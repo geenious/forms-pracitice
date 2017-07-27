47e04c39baaa4cd322d75d6efa4b14122af053be
Jeff Irwin <jeff.irwin@live.com>
2017-07-27 09:24:59 -0500

    added static public folder with index.html

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 
+// create the app
 const app = express();
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
+app.use(express.static('public'));
+
 app.listen(3000, function() {
-  console.log('Listening on port 3000');
+  console.log("What's up port 3000!");
 });
