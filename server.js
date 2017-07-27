58f25b7dcb1fde5049e2023600da8eb5933859d3
Jeff Irwin <jeff.irwin@live.com>
2017-07-27 10:18:32 -0500

    updated to include form data. server.js has mustache hack running.

--- server.js
+++ server.js
@@ -5,16 +5,22 @@ const bodyParser = require('body-parser');
 // create the app
 const app = express();
 
-app.engine('mustache', mustacheExpress());
+// mustache hack
+let mustacheInstance = mustacheExpress();
+mustacheInstance.cache = null;
+app.engine('mustache', mustacheInstance);
+
+// app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.post('/sayhello', function(req, res) {
-  console.log("I'm in your route handler");
+  // console.log("I'm in your route handler");
   console.log('The body', req.body);
   res.render('hello', req.body);
 });
